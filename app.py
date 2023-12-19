e3aa8aa35030e520321fab92fa4c1063919727c2
Piotr Brauer <brauer10@gmail.com>
2023-12-19 15:58:27 +0100

    visual changes; sqlalchemy implementation

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
 
 app = Flask(__name__)
+# access to database
+engine = create_engine('sqlite:///database.db')
+
+
 
 @app.route("/")
-def index():
+def index(): 
     return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
@@ -15,4 +23,9 @@ def login():
 def register():
     if request.method == "GET":
         return render_template("register.html")
-     
+
+@app.route("/addtask", methods=["GET", "POST"])
+def addtask():
+    if request.method == "GET":
+        return render_template("addtask.html")
+        
